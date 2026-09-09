530c1ab017e9f0ac7269da7776e4d671ec9b4585
João Suzuki <joaosuzuki98@outlook.com>
2026-09-09 00:23:30 -0300

    feat: adiciona conexão com o banco de dados

---
+++ src/sidebrain_back/core/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession, async_sessionmaker, create_async_engine
+)
+from sqlalchemy.orm import DeclarativeBase
+
+from sidebrain_back.core.constants import Env
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://"
+    f"{Env.DATABASE_USER}:{Env.DATABASE_PASSWORD}"
+    f"@{Env.DATABASE_HOST}:{Env.DATABASE_PORT}"
+    f"/{Env.DATABASE_NAME}"
+)
+
+engine = create_async_engine(DATABASE_URL, echo=Env.MODE == "dev")
+
+async_session = async_sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
+
+
+async def get_db():
+    async with async_session() as session:
+        yield session
+
+
+# Classe base para a criação de todos os models do projeto
+class Base(DeclarativeBase):
+    pass
