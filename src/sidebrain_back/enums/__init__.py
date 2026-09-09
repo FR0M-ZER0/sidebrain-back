f9492b6f1a0d223ee93ea9a9b5ce7254eabd256c
João Suzuki <joaosuzuki98@outlook.com>
2026-09-09 01:42:13 -0300

    fix: corrige nomes das classes dos enums

--- src/sidebrain_back/enums/__init__.py
+++ src/sidebrain_back/enums/__init__.py
@@ -1,27 +1,29 @@
-from sidebrain_back.enums.answer_rate import AnswerRate
-from sidebrain_back.enums.badge_criteria_enum import BadgeCriteria
-from sidebrain_back.enums.badge_progress_status_enum import BadgeProgressStatus
-from sidebrain_back.enums.badge_rarity_enum import BadgeRarity
-from sidebrain_back.enums.lesson_file_type_enum import LessonFileType
-from sidebrain_back.enums.lesson_status_enum import LessonStatus
-from sidebrain_back.enums.mission_criteria_enum import MissionCriteria
-from sidebrain_back.enums.mission_difficulty_enum import MissionDifficulty
-from sidebrain_back.enums.mission_progress_status_enum import (
-    MissionProgressStatus,
+from sidebrain_back.enums.answer_rate_enum import AnswerRateEnum
+from sidebrain_back.enums.badge_criteria_enum import BadgeCriteriaEnum
+from sidebrain_back.enums.badge_progress_status_enum import (
+    BadgeProgressStatusEnum,
 )
-from sidebrain_back.enums.step_level_enum import StepLevel
-from sidebrain_back.enums.step_status_enum import StepStatus
+from sidebrain_back.enums.badge_rarity_enum import BadgeRarityEnum
+from sidebrain_back.enums.lesson_file_type_enum import LessonFileTypeEnum
+from sidebrain_back.enums.lesson_status_enum import LessonStatusEnum
+from sidebrain_back.enums.mission_criteria_enum import MissionCriteriaEnum
+from sidebrain_back.enums.mission_difficulty_enum import MissionDifficultyEnum
+from sidebrain_back.enums.mission_progress_status_enum import (
+    MissionProgressStatusEnum,
+)
+from sidebrain_back.enums.step_level_enum import StepLevelEnum
+from sidebrain_back.enums.step_status_enum import StepStatusEnum
 
 __all__ = [
-    "MissionProgressStatus",
-    "StepStatus",
-    "LessonStatus",
-    "BadgeProgressStatus",
-    "StepLevel",
-    "MissionDifficulty",
-    "MissionCriteria",
-    "BadgeCriteria",
-    "BadgeRarity",
-    "LessonFileType",
-    "AnswerRate",
+    "MissionProgressStatusEnum",
+    "StepStatusEnum",
+    "LessonStatusEnum",
+    "BadgeProgressStatusEnum",
+    "StepLevelEnum",
+    "MissionDifficultyEnum",
+    "MissionCriteriaEnum",
+    "BadgeCriteriaEnum",
+    "BadgeRarityEnum",
+    "LessonFileTypeEnum",
+    "AnswerRateEnum",
 ]
